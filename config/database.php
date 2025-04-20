d750f8b3e65a74dfd0cdb1eb9a97ed28f875eeaf
Milla <camila.leite.oliveira@gmail.com>
2025-04-20 13:40:35 -0300

    Merge Database

--- config/database.php
+++ config/database.php
@@ -2,13 +2,22 @@
 // config/database.php
 // Vamos colocar os dados de conexão ao banco, de maneira a qual o GitHub considere seguro.
 $host = 'localhost';
-$dbname = 'nome_do_banco';
-$username = 'usuario';
-$password = 'senha';
+$port = 3307;
+$dbname = 'if0_38511592_app_sebo';
+$username = 'root';
+$password = '123';
+$password = '123';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
+        $username, 
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_TIMEOUT => 2 // Timeout curto para testes
+        ]
+    );
     return $pdo;
 } catch (PDOException $e) {
     die("Erro de conexão: " . $e->getMessage());
