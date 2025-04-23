fe91d028bd9286813903149b53caa0d2625ede26
Milla <camila.leite.oliveira@gmail.com>
2025-04-22 22:33:05 -0300

    Corrigindo o caminho do $loader

--- config/twig.php
+++ config/twig.php
@@ -1,8 +1,10 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+var_dump(__DIR__.'/../app/views');
+
 function carregarTwig() {
-    $loader = new \Twig\Loader\FilesystemLoader('../app/views');
+    $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../app/views');
     $twig = new \Twig\Environment($loader, [
         'cache' => false,
         'debug' => true,
